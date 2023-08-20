8d141c1711a9ca66c0762248d111d384ddc17e9c
Ildar Shamiev <shamief@yandex.ru>
2023-08-20 20:15:59 +0300

    add ch2-ch6

---
+++ ch5/tutor-db/database.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS ezy_course_c5;
+CREATE TABLE ezy_course_c5 (
+  course_id SERIAL PRIMARY KEY,
+  tutor_id INT NOT NULL,
+  course_name VARCHAR(140) NOT NULL,
+  posted_time TIMESTAMP DEFAULT NOW()
+);
+INSERT INTO ezy_course_c5
+  (course_id, tutor_id, course_name, posted_time)
+VALUES(1, 1, 'First course', '2021-03-17 05:40:00');
+INSERT INTO ezy_course_c5
+  (course_id, tutor_id, course_name, posted_time)
+VALUES(2, 1, 'Second course', '2021-03-18 05:45:00');
