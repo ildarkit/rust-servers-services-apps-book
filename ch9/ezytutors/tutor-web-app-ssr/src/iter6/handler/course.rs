7f7cab8c9c972c6ec76d3ea41187fe6cd33ecc74
Ildar Shamiev <shamief@yandex.ru>
2023-08-26 10:38:34 +0300

    creating resource course

--- ch9/ezytutors/tutor-web-app-ssr/src/iter6/handler/course.rs
+++ ch9/ezytutors/tutor-web-app-ssr/src/iter6/handler/course.rs
@@ -1,12 +1,38 @@
 use actix_web::{web, Error, HttpResponse, Result};
 use crate::iter6::state::AppState;
+use crate::model::{NewCourse, NewCourseResponse};
+use serde_json::json;
 
 pub async fn handle_insert_course(
     _tmpl: web::Data<tera::Tera>,
     _app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+    params: web::Json<NewCourse>,
 ) -> Result<HttpResponse, Error> {
-    println!("Got insert request");
-    Ok(HttpResponse::Ok().body("Got insert request"))
+    let tutor_id = path.into_inner();
+    let new_course = json!({
+        "tutor_id": tutor_id,
+        "course_name": &params.course_name,
+        "course_description": &params.course_description,
+        "course_format": &params.course_format,
+        "course_structure": &params.course_structure,
+        "course_duration": &params.course_duration,
+        "course_price": &params.course_price,
+        "course_language": &params.course_language,
+        "course_level": &params.course_level,
+    });
+    let awc_client = awc::Client::default();
+    let res = awc_client
+        .post("http://localhost:3000/courses/")
+        .send_json(&new_course)
+        .await
+        .unwrap()
+        .body()
+        .await?;
+    println!("Finished call: {:?}", res);
+    let course_response: NewCourseResponse = serde_json::from_str(
+        &std::str::from_utf8(&res)?)?;
+    Ok(HttpResponse::Ok().json(course_response))
 }
 
 pub async fn handle_update_course(
--- ch9/ezytutors/tutor-web-app-ssr/src/iter6/model.rs
+++ ch9/ezytutors/tutor-web-app-ssr/src/iter6/model.rs
@@ -30,3 +30,30 @@ pub struct User {
     pub tutor_id: Option<i32>,
     pub user_password: String,
 }
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NewCourse {
+    pub course_name: String,
+    pub course_description: String,
+    pub course_format: String,
+    pub course_duration: String,
+    pub course_structure: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct NewCourseResponse {
+    pub course_id: i32,
+    pub tutor_id: i32,
+    pub course_name: String,
+    pub course_description: String,
+    pub course_format: String,
+    pub course_duration: String,
+    pub course_structure: Option<String>,
+    pub course_price: Option<i32>,
+    pub course_language: Option<String>,
+    pub course_level: Option<String>,
+    pub posted_time: String,
+}
